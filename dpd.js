e7d013416c5d3189639e24d60d1ea70e43b77f4f
Ruairi O'Brien <ruairitobrien@gmail.com>
2015-11-07 00:00:27 +0000

    Upated env var for Mongo URL

--- dpd.js
+++ dpd.js
@@ -2,7 +2,7 @@ var deployd = require('deployd');
 var options = {
 		port: 80,
 		db: {
-			connectionString: provess.env.mongoUrl
+			connectionString: provess.env.MONGO_URL
 		    }
 		}
 var dpd = deployd(options);
