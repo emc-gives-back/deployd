4876ae867b445404b48ff258183da84eb43f6680
Ruairi O Brien <ruairitobrien@gmail.com>
2015-11-09 10:06:23 +0000

    Updated models a bit. Made the mongodb url work for local instance too

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ var port = process.env.PORT || 3000;
 var url = require('url');
 var db_url = url.parse(process.env.MONGO_URL || "mongodb://:@localhost:27017/emc_give_back");
 
+var credentials = {
+    "username": db_url.auth.split(':')[0],
+    "password": db_url.auth.split(':')[1]
+};
+
+var db = {
+    "host": db_url.hostname,
+    "port": parseInt(db_url.port),
+    "name": db_url.pathname.slice(1)
+};
+
+if (credentials.username) {
+    db.credentials = credentials
+}
+
 var options = {
     port: port,
-    db: {
-        "host": db_url.hostname,
-        "port": parseInt(db_url.port),
-        "name": db_url.pathname.slice(1),
-        "credentials": {
-            "username": db_url.auth.split(':')[0],
-            "password": db_url.auth.split(':')[1]
-        }
-    }
+    db: db
 };
 
 console.log(JSON.stringify(options));
@@ -22,13 +29,13 @@ console.log(JSON.stringify(options));
 var server = deployd(options);
 server.listen();
 
-server.on('listening', function() {
-  console.log("Server is listening on " + port);
+server.on('listening', function () {
+    console.log("Server is listening on " + port);
 });
 
-server.on('error', function(err) {
-  console.error(err);
-  process.nextTick(function() { // Give the server a chance to return an error
-    process.exit();
-  });
+server.on('error', function (err) {
+    console.error(err);
+    process.nextTick(function () { // Give the server a chance to return an error
+        process.exit();
+    });
 });
